18b29c5b903d95e6359b8d3b248a2b4ed52fd154
Sol Najle <sgnajle@gmail.com>
2025-07-11 14:52:24 +0200

    Changed Promise to observable and also @for instead of @defer

--- src/app/formatges/formatge.service.ts
+++ src/app/formatges/formatge.service.ts
@@ -1,19 +1,17 @@
 import { Injectable } from "@angular/core";
 import { Formatge } from "./formatge.interface";
+import { Observable, from } from "rxjs";
 
 @Injectable({
   providedIn: "root",
 })
 export class FormatgeService {
     url ="http://localhost:3000/formatges"; 
-    async getFormatges(): Promise<Formatge[]> {
-        const response = await fetch(this.url);  // Replace with your actual API endpoint
-        if (!response.ok) {                                                // Check if the request was successful
-          throw new Error("Failed to fetch formatges");
-        }
-        const data = await response.json();
-        return data as Formatge[];
-      }
+  getFormatges(): Observable<Formatge[]> {
+    return from(fetch(this.url)
+      .then(response => response.json())
+      .then(data => data as Formatge[]));
+  }
     async getFormatgeById(id: string): Promise<Formatge> {
         const response = await fetch(`${this.url}?id=${id}`);  // Replace with your actual API endpoint
         if (!response.ok) {                                                // Check if the request was successful
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -3,29 +3,23 @@ import { Formatge } from '../formatges/formatge.interface';
 import { FormatgeService } from '../formatges/formatge.service';
 import { CommonModule } from '@angular/common';
 import { FormatgeCard } from '../formatges/formatge-card';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-home',
   imports: [CommonModule, FormatgeCard],
   template: `<h1>Formatges</h1>
   <section class="home-list"> 
-    @defer (when formatgeList.length > 0){
-        <app-formatge-card  *ngFor="let formatge of formatgeList" [formatge]="formatge"></app-formatge-card> 
+    @for (formatge of formatgeList$ | async; track formatge.id) {
+      <app-formatge-card [formatge]="formatge"></app-formatge-card>
     }
   </section>`,
   styleUrl: './home.css',
 })
 export class Home {
-  formatgeList: Formatge[] = [];
-  formatgeService: FormatgeService = inject(FormatgeService);
+  formatgeList$: Observable<Formatge[]>;
+  formatgeService = inject(FormatgeService);
+
   constructor() {
-    this.formatgeService
-      .getFormatges()
-      .then((formatges: Formatge[]) => {
-        this.formatgeList = formatges;
-        console.log('Formatges fetched successfully:', this.formatgeList);
-      })
-      .catch((error: Error) => {
-        console.error('Error fetching formatges:', error);
-      });
+    this.formatgeList$ = this.formatgeService.getFormatges();
   }
 }
